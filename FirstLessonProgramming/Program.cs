e74c72efb38ac1875cf6abbea44b00dd48c94a66
Rasim Alagezov <r.alagezov@gmail.com>
2022-04-20 19:48:33 +0400

    Project initially added

---
+++ FirstLessonProgramming/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace FirstLessonProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            
+            Console.WriteLine("Please enter First number");
+            while (true)
+            {
+                try
+                {
+                    a = Convert.ToInt16(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You can enter only a number");
+                }
+            }
+            Console.WriteLine("Please eneter second number");
+            int b;
+            while (true)
+            {
+                try
+                {
+                    b = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You can enter only a number");
+                }
+            }
+            
+            int sum = Sum(a, b);
+            Console.WriteLine("Sum is :" + sum);
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
