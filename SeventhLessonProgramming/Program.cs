b1416cfadaf38c944bba4a028dfa26fbb08c77c0
Rasim Alagezov <r.alagezov@gmail.com>
2022-04-26 15:01:47 +0400

    Lesson 8

---
+++ SeventhLessonProgramming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SeventhLessonProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> candles = new List<int> { 1, 2, 5, 5 };
+
+           
+            Console.WriteLine(candles.Count(x => x == candles.Max()));
+           
+            //Console.WriteLine(arr.Sum()-arr.Max() + " "+ (arr.Sum()-arr.Min()));
+            
+        }
+    }
+}
