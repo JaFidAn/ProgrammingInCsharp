3baa3443e28b68553a1105f80d0736777f9b5a68
Rasim Alagezov <r.alagezov@gmail.com>
2022-04-29 17:01:37 +0400

    Lesson 14 Exceptions

---
+++ LessonFourteeenProgramming/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace LessonFourteeenProgramming
+{
+    internal class Program
+    {
+        const string LogFilePath = "D:\\C#\\LogToFile\\Logs.txt";
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Please enter a number");
+            int number=0;
+            try
+            {
+                number = int.Parse(Console.ReadLine());
+               
+            }
+            catch (DivideByZeroException ex)
+            {
+                File.AppendAllText(LogFilePath, DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + "\n" + ex.ToString() + "\n\n");
+            }
+            catch (Exception ex)
+            {
+            
+                File.AppendAllText(LogFilePath, DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + "\n" + ex.ToString() + ex.Message+"\n\n");
+                throw new OperationRestictedException("This is not allowed", ex);
+            }
+            finally
+            {
+                Console.WriteLine("This block will always run");
+            }
+            Console.WriteLine(number);
+            
+        }
+    }
+}
